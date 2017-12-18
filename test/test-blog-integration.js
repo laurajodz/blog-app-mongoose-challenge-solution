4e4158ca031fd40c766c7157bbf455e4ca665689
Laura Jodz <laurajodz@gmail.com>
2017-12-17 21:16:03 -0800

    updated test file

---
+++ test/test-blog-integration.js
@@ -0,0 +1,219 @@
+'use strict';
+
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const faker = require('faker');
+const mongoose = require('mongoose');
+
+const should = chai.should();
+
+const {Post} = require('../models');
+const {app, runServer, closeServer} = require('../server');
+const {TEST_DATABASE_URL} = require('../config');
+
+chai.use(chaiHttp);
+
+function seedPostData() {
+  console.info('seeding post data');
+  const seedData = [];
+
+  for (let i = 1; i <= 10; i++) {
+    seedData.push({
+      author: {
+        firstName: faker.name.firstName(),
+        lastName: faker.name.lastName()
+      },
+      title: faker.lorem.sentence(),
+      content: faker.lorem.paragraph()
+    });
+  }
+  return Post.insertMany(seedData);
+}
+
+
+function tearDownDb() {
+  console.warn('Deleting database');
+  return mongoose.connection.dropDatabase();
+}
+
+describe('Blog Posts API resource', function() {
+
+  before(function() {
+    return runServer(TEST_DATABASE_URL);
+  });
+
+  beforeEach(function() {
+    return seedPostData();
+  });
+
+  afterEach(function() {
+    return tearDownDb();
+  });
+
+  after(function() {
+    return closeServer();
+  });
+
+
+  describe('GET endpoint', function() {
+
+    it('should return all existing blog posts', function() {
+      // strategy:
+      //    1. get back all posts returned by by GET request to `/posts`
+      //    2. prove res has right status, data type
+      //    3. prove the number of posts we got back is equal to number
+      //       in db.
+      //
+      // need to have access to mutate and access `res` across
+      // `.then()` calls below, so declare it here so can modify in place
+      let res;
+      return chai.request(app)
+        .get('/posts')
+        .then(function(_res) {
+          // so subsequent .then blocks can access resp obj.
+          res = _res;
+          res.should.have.status(200);
+          // otherwise our db seeding didn't work
+          res.body.should.have.length.of.at.least(1);
+          return Post.count();
+        })
+        .then(function(count) {
+          res.body.should.have.length.of(count);
+        });
+    });
+
+
+    it('should return blog posts with right fields', function() {
+      // Strategy: Get back all posts, and ensure they have expected keys
+      let resPost;
+      return chai.request(app)
+        .get('/posts')
+        .then(function(res) {
+          res.should.have.status(200);
+          res.should.be.json;
+          res.body.should.be.a('array');
+          res.body.should.have.length.of.at.least(1);
+
+          res.body.forEach(function(post) {
+            post.should.be.a('object');
+            post.should.include.keys(
+              'id', 'title', 'author', 'content', 'created');
+          });
+          resPost = res.body[0];
+          return Post.findById(resPost.id);
+        })
+        .then(function(post) {
+          resPost.title.should.equal(post.title);
+          resPost.author.should.equal(post.author);
+          resPost.content.should.equal(post.content);
+        });
+    });
+  });
+
+  describe('POST endpoint', function() {
+    // strategy: make a POST request with data,
+    // then prove that the post we get back has
+    // right keys, and that `id` is there (which means
+    // the data was inserted into db)
+    it('should add a new post', function() {
+
+      const newPost = {
+        title: faker.lorem.sentence(),
+        author: {
+          firstName: faker.name.firstName(),
+          lastName: faker.name.lastName(),
+        },
+        content: faker.lorem.paragraph()
+      }
+      return chai.request(app)
+        .post('/posts')
+        .send(newPost)
+        .then(function(res) {
+          res.should.have.status(201);
+          res.should.be.json;
+          res.body.should.be.a('object');
+          res.body.should.include.keys(
+            'id', 'title', 'author', 'content', 'created');
+          res.body.title.should.equal(newPost.title);
+          // Mongo should have created id on insertion
+          res.body.id.should.not.be.null;
+          res.body.author.should.equal(
+            `${newPost.author.firstName} ${newPost.author.lastName}`);
+          res.body.content.should.equal(newPost.content);
+          return Post.findById(res.body.id);
+        })
+        .then(function(post) {
+          post.title.should.equal(newPost.title);
+          post.author.firstName.should.equal(newPost.author.firstName);
+          post.author.lastName.should.equal(newPost.author.lastName);
+          post.content.should.equal(newPost.content);
+        });
+    });
+  });
+
+  describe('PUT endpoint', function() {
+    // strategy:
+    //  1. Get an existing post from db
+    //  2. Make a PUT request to update that post
+    //  3. Prove post returned by request contains data we sent
+    //  4. Prove post in db is correctly updated
+    it('should update fields you send over', function() {
+      const updateData = {
+        title: 'Christmas Holiday Travel',
+        author: {
+          firstName: 'Laura',
+          lastName: 'Jodz'
+        }
+      };
+      return Post
+        .findOne()
+        .then(function(post) {
+          updateData.id = post.id;
+          // make request then inspect it to make sure it reflects
+          // data we sent
+          return chai.request(app)
+            .put(`/posts/${post.id}`)
+            .send(updateData);
+        })
+        .then(function(res) {
+          res.should.have.status(204);
+          return Post.findById(updateData.id);
+        })
+        .then(function(post) {
+          post.title.should.equal(updateData.title);
+          post.author.firstName.should.equal(updateData.author.firstName);
+          post.author.lastName.should.equal(updateData.author.lastName);
+        });
+    });
+  });
+
+  describe('DELETE endpoint', function() {
+    // strategy:
+    //  1. get a restaurant
+    //  2. make a DELETE request for that restaurant's id
+    //  3. assert that response has right status code
+    //  4. prove that restaurant with the id doesn't exist in db anymore
+    it('delete a post by id', function() {
+
+      let blogPost;
+
+      return Post
+        .findOne()
+        .then(function(_post) {
+          blogPost = _post;
+          return chai.request(app).delete(`/posts/${post.id}`);
+        })
+        .then(function(res) {
+          res.should.have.status(204);
+          return Post.findById(post.id);
+        })
+        .then(function(_post) {
+          // when a variable's value is null, chaining `should`
+          // doesn't work. so `_restaurant.should.be.null` would raise
+          // an error. `should.be.null(_restaurant)` is how we can
+          // make assertions about a null value.
+          should.not.exist(_post);
+        });
+    });
+  });
+});
